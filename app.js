7b59352bddec0c9de63b8b701636c194b1ee076a
Muskaankamran <muskaankamran494@gmail.com>
2025-11-25 01:31:26 -0800

    half

--- app.js
+++ app.js
@@ -1,187 +1,183 @@
 var foreverProducts = {
     skincare: {
-        CeraVe: {
-            ' Lotion': {
-                img: "https://www.cerave.com/-/media/project/loreal/bran…n-us&w=1024&hash=31BCD8A509C801DBEA011177A6FEEE78",
+        bdy: {
+            "Lotion": {
+                img: "https://bdy.treehutshea.com/cdn/shop/files/BDY148.jpg?v=1745510707&width=1000",
                 price: "$17.99",
-                description: "CeraVe Intensive Moisturizing Lotion is a gentle, lightweight formula that works to repair and maintain the skin barrier. The formula also features 5% Hydro-Urea to help replenish the skin's natural moisturizing factors that are essential to attracting and retaining optimal moisture to deeply hydrate dry and very dry, itchy skin."
+                description: "bdy. Restoring Weightless Body Lotion with Glycerin + Hyaluronic Acid absorbs quickly to help lock in moisture for glowing skin without a greasy feel."
             },
-            'Facial serums': {
-                img: "https://www.cerave.com/-/media/project/loreal/bran…140a&w=1024&hash=27F14DC5E5C7746BEA2F7B1A346600F4",
+            "Facial serums": {
+                img: "https://bdy.treehutshea.com/cdn/shop/files/BDY146.jpg?v=1745510547&width=1000",
                 price: "$21.99",
-                description: "Developed with dermatologists, CeraVe Resurfacing Retinol Serum helps smooth your skin's texture by reducing the appearance of post-acne marks, minimizing the look of pores and improving your skin’s surface."
+                description: "bdy. Mini Brightening Dual Phase Body Serum Mist with Niacinamide + Squalane is a silky, non-greasy serum mist, perfect for on-the-go dewy skin."
             },
-            'Sun screens': {
-                img: "https://www.cerave.com/-/media/project/loreal/bran…1cea&w=1024&hash=4EE999FC71524F74629A8F246EB09377",
-                price: "$17.99",
-                description: "Developed with dermatologists, CeraVe Hydrating Sheer Sunscreen Broad Spectrum SPF 30 combines mineral and chemical sun filters to help effectively reflect and absorb the sun's damaging rays.",
+            "oil body wash": {
+                img: "https://bdy.treehutshea.com/cdn/shop/files/BDY138.jpg?v=1745510678&width=1000",
+                price: "$20.00",
+                description: "bdy. Foaming Oil Hydrating Body Wash with Hyaluronic Acid + Squalane is a velvety oil-to-foam cleanser that helps hydrate, balance and soothe skin."
             },
-        },
 
-
-
-        Bathnbody: {
-            'body wash': {
-                img: " https://www.bathandbodyworks.com/dw/image/v2/BBDL_…efault/dw74b45243/hires/028005114.jpg?sw=400&q=60",
-                price: "$16.95",
-                description: "Warm your heart a thousand times over with this dazzling scent. Like raising a glass of bubbly in celebration, this fragrance is both captivating and joyful. So, let the effervescent energy flow over you and take a moment to revel in this light, bright floral beauty.",
-            }
-        },
-        ' Face wash': {
-            img: "https://www.bathandbodyworks.com/dw/image/v2/BBDL_…efault/dwc88a3f9d/hires/026661312.jpg?sw=400&q=60",
-            price: "$13.95",
-            description: "What it smells like: a breath of fresh, clean and calming air.",
-        },
-        ' Hand soap': {
-            img: "https://www.bathandbodyworks.com/dw/image/v2/BBDL_…efault/dw4e79b8fc/hires/028023429.jpg?sw=400&q=60",
-            price: "$8.95",
-            description: 'Savor the softness of lush florals, and immerse yourself in the luxury of a thousand rose petals at your fingertips'
-        },
-
-        lorealparis: {
-            'mosturizer': {
-                img: "https://www.lorealparisusa.com/-/media/project/lor…care_rvl_tp_day_48g__primary_071249696057_av1.png",
-                price: "$29.99",
-                description: "Revitalift Anti-Aging Face Moisturizer immediately hydrates skin, while working overtime to visibly reduce wrinkles, firm skin and brighten in 1 week. Powered with not just 1, but 3 derm-recommended ingredients: Pro-Retinol, Hyaluronic Acid and Vitamin C."
-            },
-            'powder': {
-                img: "https://www.lorealparisusa.com/-/media/project/lor…atural-beige-warm/08-11-23-en/071249016442-t1.png",
-                price: "$12.99",
-                description: `A pressed powder that provides buildable coverage while minimizing pores and fine lines. This powder foundation is non-cakey, oil-free and non-comedogenic.
-Get a perfect match with True Match Super- Blendable Oil Free Makeup Powder.This micro- fine pressed powder provides buildable, long- lasting coverage.Choose your True Match shade to find one that best complements your skin tone and undertone." ,
-           `},
-            'serum': {
-                img: "https://www.lorealparisusa.com/-/media/project/lor…idnight-serum/atf-v2-2024/071249630167_t1-new.png",
-                price: "$43.99",
-                description: "A lightweight facial serum that’s setting a new standard for luxury serums. The formula is non-greasy, non-sticky, paraben-free, mineral oil-free, allergy tested, suitable for sensitive skin and won’t clog pores.",
-            },
-            NordStrom: {
-                'cleansers': {
-                    img: "https://n.nordstrommedia.com/it/99f6a9ea-279a-451e-a3b8-3c46bc0b1cde.jpeg?h=368&w=240&dpr=2",
-                    price: "$34",
-                    description: "This rich and creamy cleanser with nourishing rosehip oil and soothing Rose de France delivers a voluminous foam to gently remove makeup (even waterproof), dissolve impurities and leave the skin soft and supple.",
+            Bathnbody: {
+                "body wash": {
+                    img: "https://www.bathandbodyworks.com/...correct-url-here.jpg",
+                    price: "$16.95",
+                    description: "Warm your heart a thousand times over..."
                 },
-                'moisturizing cream': {
-                    img: "https://n.nordstrommedia.com/it/aaa16fd1-0a3c-40d9-b7a7-bcf0f60f938c.jpeg?crop=pad&w=1950&h=2990",
-                    price: "$100.00 ",
-                    description: "Powered by visible youth-regenerating Miracle Broth™, this iconic moisturizer begins to activate repair immediately. In hours, skin feels firmer, looks smoother and more radiant*."
+                "Face wash": {
+                    img: "https://www.bathandbodyworks.com/...correct-url-here.jpg",
+                    price: "$13.95",
+                    description: "What it smells like: a breath of fresh, clean and calming air."
                 },
-                'serum': {
-                    img: "https://n.nordstrommedia.com/it/f16df3d7-eac2-435a-8005-176f1ed2e85b.jpeg?crop=pad&w=780&h=1196",
-                    price: "$99.00",
-                    description: " This serum utilizes bio-technology, lactic acid and an ultra-stable vitamin C complex, which work together to fight the appearance of skin aging before it starts. The optimized delivery system—peptide and AHA—exfoliates and reduces unbalanced pigmentation."
+                "Hand soap": {
+                    img: "https://www.bathandbodyworks.com/...correct-url-here.jpg",
+                    price: "$8.95",
+                    description: "Savor the softness of lush florals..."
                 }
             },
-        },
+
+            lorealparis: {
+                "sunscreen": {
+                    img: "https://www.lorealparisusa.com/-/media/project/lor…care_rvl_tp_day_48g__primary_071249696057_av1.png",
+                    price: "$29.99",
+                    description: "Formulated with 3 of the top dermatologist-proven ingredients - Pro-Retinol, Hyaluronic Acid, and Vitamin C"
+                },
+                "moisturaizer": {
+                    img: "https://www.lorealparisusa.com/-/media/project/lor…-filler-moisture-bounce-serum/071249699638-t1.png",
+                    price: "$12.99",
+                    description: "Use this fast-absorbing serum alone or under makeup for a healthy, hydrated glow"
+                },
+                "night cream": {
+                    img: "https://www.lorealparisusa.com/-/media/project/lor…one-collagen-expert-night-moisturizer-primary.png",
+                    price: "$43.99",
+                    description: `Age Perfect Collagen Expert Night Moisturizer is an intensely hydrating night cream for face, made with powerful Collagen Peptides + Niacinamide to help retighten, rehydrate and firm maturing skin."
+                `},
+            },
+  
+            NordStrom: {
+                "cleansers": {
+                    img: "https://n.nordstrommedia.com/it/e3cd70db-a775-4649-aeae-c031905a5146.jpeg?crop=pad&w=1950&h=2990",
+                    price: "$34.00",
+                    description: "Transforming from a velvety cleansing cream into a finely-textured lather, it treats skin to a luxurious sensory experience. With continued use, this cleanser helps skin look and feel deeply cleansed, thoroughly moisturized, soft and smooth."
+                },
+                "moisturizing cream": {
+                    img: "https://n.nordstrommedia.com/it/71507e01-61ce-4bb2-a6cb-4f8a8a18df74.jpeg?crop=pad&w=780&h=1196" ,
+                    price: "$100.00",
+                    description: "An silky-soft moisturizer that delivers all-day healing moisture and profound rejuvenation for visibly plumper, healthier, firmer, lifted-looking skin."
+              },
+                "serum": {
+                    img: "https://n.nordstrommedia.com/it/eec04cbb-d19f-4a11-a718-32a3fe711438.jpeg?crop=pad&w=780&h=1196",
+                    price: "$99.00",
+                    description: " A powerful dark spot–fighting serum with that immediately boosts radiance and hydrates for 48 hours."
+                }
+            }
+        }
     },
+
     haircare: {
         sephora: {
-            'shampoo': {
+            "shampoo": {
                 img: "https://www.sephora.com/productimages/sku/s2319820-main-zoom.jpg?imwidth=612",
                 price: "$34.00",
                 description: "Detox Clarifying Shampoo with Apple Cider Vinegar"
             },
-            'conditioner': {
+            "conditioner": {
                 img: "https://www.sephora.com/productimages/sku/s2390821-main-zoom.jpg?imwidth=315",
                 price: "$37.00",
-                description: "Hydrate Sheer Conditioner for Fine Hair",
+                description: "Hydrate Sheer Conditioner for Fine Hair"
             },
-            'scalp scrub': {
+            "scalp scrub": {
                 img: "https://www.sephora.com/productimages/sku/s2815132-main-zoom.jpg?imwidth=315",
                 price: "$40.00",
-                description: "A clarifying shampoo that revitalizes scalp lacking in tone with gentle, natural surfactants and scrub particles, leaving scalp and hair purified and healthy.",
+                description: "A clarifying shampoo that revitalizes scalp..."
             }
         },
+
         tresemme: {
-            'shampoo': {
-                img: "https://assets.unileversolutions.com/v1/132852805.png?im=AspectCrop=(400,400);Resize=(400,400)",
+            "shampoo": {
+                img: "https://assets.unileversolutions.com/v1/132852805.png",
                 price: "$6.98",
-                description: "Our Keratin Smooth Blowout Heat Protect Spray is designed to perfect your iconic blowout while protecting against heat up to 450° F."
+                description: "Keratin Smooth Blowout Heat Protect Spray..."
             },
-            'conditioner': {
-                img: "https://assets.unileversolutions.com/v1/133276357.png?im=AspectCrop=(400,400);Resize=(400,400)",
+            "conditioner": {
+                img: "https://assets.unileversolutions.com/v1/133276357.png",
                 price: "$12.45",
-                description: "This deep conditioning hair treatment, made with a unique Volume Control Complex and Silk proteins, helps keep your hair from falling flat and makes it more manageable without weighing it down"
+                description: "Deep conditioning hair treatment..."
             },
-            'hair mousse': {
-                img: "https://assets.unileversolutions.com/v1/132563504.png?im=AspectCrop=(400,400);Resize=(400,400)",
+            "hair mousse": {
+                img: "https://assets.unileversolutions.com/v1/132563504.png",
                 price: "$13.45",
-                description: " With up to 72H frizz defense, this product ensures your hair remains sleek and manageable all day long, no matter the weather conditions."
+                description: "Provides 72H frizz defense..."
             }
         },
+
         seen: {
-            'hydrating shampoo': {
+            "hydrating shampoo": {
                 img: "https://helloseen.com/cdn/shop/files/0034_Shampoo_FragranceFree_Front-_REG_720x.webp?v=1746082555",
-                price: "$ 29.75",
-                description: "Provides a luxurious lather and helps promote stronger, smoother, healthier-looking hair."
+                price: "$29.75",
+                description: "Luxurious lather for stronger, smoother hair."
             },
-            'magic serum': {
+            "magic serum": {
                 img: "https://helloseen.com/cdn/shop/files/0063_MagicSerum_Front_720x.webp?v=1746080061",
-                price: "$ 37.20",
-                description: "Reduces frizz, adds shine, nourishes and provides hair with protection against heat up to 450º F.",
+                price: "$37.20",
+                description: "Reduces frizz, adds shine..."
             },
-            'body wash': {
-                img: "https://helloseen.com/cdn/shop/files/0049_BodyWash…eea-497c-86d4-55089a72b37f_720x.webp?v=1746084345",
-                price: "$ 24.8",
-                description: "This gentle yet effective award-winning formula leaves skin feeling fresh and balanced without clogging pores or irritating skin."
+            "conditioner": {
+                img: "https://helloseen.com/cdn/shop/files/0045_Leave-InConditioner_Front_720x.webp?v=1748261699",
+                price: "$24.80",
+                description: "Lightweight hydration with UV protection."
             }
-
         },
+
         dove: {
-            'shampoo': {
-                img: "	https://assets.unileversolutions.com/v1/130260752.png?im=AspectCrop=(520,520);Resize=(520,520)",
+            "shampoo": {
+                img: "https://assets.unileversolutions.com/v1/130260752.png",
                 price: "$1.31",
-                description: "For manageable and silky hair that feels beautifully soft, this Dove shampoo helps protect hair from daily wear and tear."
+                description: "For manageable and silky hair..."
             },
-            'conditioner':{
-                img: "https://assets.unileversolutions.com/v1/130260715.png?im=AspectCrop=(520,520);Resize=(520,520)",
-                price: "$ 2.96",
-                description: "Dove Daily Moisture Light Conditioner, made with Oxyfusion ingredients.",
+            "conditioner": {
+                img: "https://assets.unileversolutions.com/v1/130260715.png",
+                price: "$2.96",
+                description: "Made with Oxyfusion ingredients."
             },
-            'hair treatment':{
-                img: "https://assets.unileversolutions.com/v1/113805421.png?im=AspectCrop=(520,520);Resize=(520,520)",
-                price: "$ 2.59",
-                description: "Transform your everyday hair wash into a moment of intensive damaged hair repair with Dove 1 Minute Super Conditioner. ",
-            },
-        },
-
-
-    },
-    babycare: {
-
-    },
-    fragrance: {
-
+            "hair treatment": {
+                img: "https://assets.unileversolutions.com/v1/113805421.png",
+                price: "$2.59",
+                description: "1 Minute Super Conditioner for damaged hair."
+            }
+        }
     },
 
+    babycare: {},
+    fragrance: {}
+};
 
-}
 var productMenu = document.getElementById("productMenu");
 var allProducts = document.getElementById("allProducts");
-for (product in foreverProducts) {
-//   console.log(product);
-//   productMenu.innerHTML += `
-//             <option value="${product}">${product.toUpperCase()}</option>
 
-//     `;
-  for (brand in foreverProducts[product]) {
-   
-    productMenu.innerHTML += `
- <div class="col">
-          <div class="card">
-            <img
-              src=${foreverProducts[product][brand].src}
-              class="card-img-top"
-              alt="..."
-            />
-            <div class="card-body">
-              <h5 class="card-title">${foreverProducts[product][brand].name}</h5>
-              <p class="card-text">${foreverProducts[product][brand].description}</p>
-            </div>
-            <div class="card-footer">
-              <h5 class="price">${foreverProducts[product][brand].price}/- </h5>
-            </div>
-          </div>
-        </div>`;
-  }
+for (let category in foreverProducts) {
+    for (let brand in foreverProducts[category]) {
+        for (let item in foreverProducts[category][brand]) {
+
+            let product = foreverProducts[category][brand][item];
+
+            productMenu.innerHTML += `
+                        <div class= "col">
+                            <div class="card">
+                                <img
+                                    src="${product.img}"
+                                    class="card-img-top"
+                                    alt="${item}"
+                                />
+                                <div class="card-body">
+                                    <h5 class="card-title">${item}</h5>
+                                    <p class="card-text">${product.description}</p>
+                                </div>
+                                <div class="card-footer">
+                                    <h5 class="price">${product.price}</h5>
+                                </div>
+                            </div>
+        </div >
+    `;
+        }
+    }
 }
